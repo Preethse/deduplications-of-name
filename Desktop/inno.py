aad0bf26052ed868c21048a95c48a2ac978c0e57
preetham <preethamp0197@gmail.com>
2018-02-24 00:57:02 +0530

    Initial Commit

---
+++ Desktop/inno.py
@@ -0,0 +1,47 @@
+import re, math
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+from nltk.corpus import wordnet as wn
+
+stop = stopwords.words('english')
+
+WORD = re.compile(r'\w+')
+stemmer = PorterStemmer()
+
+def get_cosine(vec1, vec2):
+    # print vec1, vec2
+    intersection = set(vec1.keys()) & set(vec2.keys())
+    numerator = sum([vec1[x] * vec2[x] for x in intersection])
+
+    sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+    sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+    denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+    if not denominator:
+        return 0.0
+    else:
+        return float(numerator) / denominator
+
+def text_to_vector(text):
+    words = WORD.findall(text)
+    a = []
+    for i in words:
+        for ss in wn.synsets(i):
+            a.extend(ss.lemma_names())
+    for i in words:
+        if i not in a:
+            a.append(i)
+    a = set(a)
+    w = [stemmer.stem(i) for i in a if i not in stop]
+    return Counter(w)
+
+def get_similarity(a, b):
+    a = text_to_vector(a.strip().lower())
+    b = text_to_vector(b.strip().lower())
+
+    return get_cosine(a, b)
+
+
+c=get_similarity('ducan fereto', 'ducan b fereto')
+print(c)
